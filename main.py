c01557af995b06734ee34c4a6f9c44a7dea0d68b
Gravity-Developer <piderking8@gmail.com>
2023-08-21 19:42:27 -0700

    Annotated Image

--- main.py
+++ main.py
@@ -2,27 +2,51 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 from os.path import abspath, join as path_join
-BASE_MODEL_PATH = abspath("./models")
-IMAGE_PATH = abspath("./data/images")
-MEDIAPIPE_MODELS_PATH = abspath("./models/mediapipe")
 
-model_path = path_join(MEDIAPIPE_MODELS_PATH, "pose_landmarker_full.task")
-
-BaseOptions = mp.tasks.BaseOptions
-PoseLandmarker = mp.tasks.vision.PoseLandmarker
-PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
-VisionRunningMode = mp.tasks.vision.RunningMode
-
-options = PoseLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=VisionRunningMode.IMAGE)
-
-with PoseLandmarker.create_from_options(options) as landmarker:
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
+import cv2
 
 
-    # Load the input image from an image file.
-    mp_image = mp.Image.create_from_file(path_join(IMAGE_PATH, "ski.jpg"))
+def draw_landmarks_on_image(rgb_image, detection_result):
+  pose_landmarks_list = detection_result.pose_landmarks
+  annotated_image = np.copy(rgb_image)
 
-    pose_landmarker_result = landmarker.detect(mp_image)
+  # Loop through the detected poses to visualize.
+  for idx in range(len(pose_landmarks_list)):
+    pose_landmarks = pose_landmarks_list[idx]
 
-    # Try Messing Around with the landmarks # https://github.com/googlesamples/mediapipe/blob/main/examples/pose_landmarker/python/%5BMediaPipe_Python_Tasks%5D_Pose_Landmarker.ipynb (Try using some of that)
+    # Draw the pose landmarks.
+    pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    pose_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in pose_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      pose_landmarks_proto,
+      solutions.pose.POSE_CONNECTIONS,
+      solutions.drawing_styles.get_default_pose_landmarks_style())
+  return annotated_image
+
+base_options = python.BaseOptions(model_asset_path='./models/mediapipe/pose_landmarker_full.task')
+options = vision.PoseLandmarkerOptions(
+    base_options=base_options,
+    output_segmentation_masks=True)
+detector = vision.PoseLandmarker.create_from_options(options)
+
+# STEP 3: Load the input image.
+image = mp.Image.create_from_file("./data/images/ski.jpg")
+
+# STEP 4: Detect pose landmarks from the input image.
+detection_result = detector.detect(image)
+
+# STEP 5: Process the detection result. In this case, visualize it.
+annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+cv2.imshow('', cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+
+# (this is necessary to avoid Python kernel form crashing)
+cv2.waitKey(0)
+  
+# closing all open windows
+cv2.destroyAllWindows()
